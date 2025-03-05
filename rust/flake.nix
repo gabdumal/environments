717b71e0489096d6e390ef32d2ba9179f2c427cc
Gabriel Malosto <professional@gabdumal.com>
2025-03-05 16:57:13 -0300

    Add dependencies for Dioxus on Rust environment

--- rust/flake.nix
+++ rust/flake.nix
@@ -37,11 +37,27 @@
         devShells.default =
           with pkgs;
           mkShell {
-            buildInputs = [
-              openssl
+            nativeBuildInputs = with pkgs; [
               pkg-config
-              eza
-              fd
+              gobject-introspection
+              cargo
+              cargo-tauri
+              nodejs
+            ];
+
+            buildInputs = with pkgs; [
+              at-spi2-atk
+              atkmm
+              cairo
+              gdk-pixbuf
+              glib
+              gtk3
+              harfbuzz
+              librsvg
+              libsoup_3
+              pango
+              webkitgtk_4_1
+              openssl
               (rust-bin.fromRustupToolchainFile ./rust-toolchain.toml)
             ];
 
