29dda3ef95e071b02212f5426d6902ae140881bd
Gabriel Malosto <professional@gabdumal.com>
2025-04-28 11:44:38 -0300

    Fix versions on C and C++ env

--- c_cpp/flake.nix
+++ c_cpp/flake.nix
@@ -5,7 +5,8 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
   };
 
-  outputs = { self, nixpkgs, ... }:
+  outputs =
+    { self, nixpkgs, ... }:
     let
       supportedSystems = [
         "x86_64-linux"
@@ -14,40 +15,43 @@
         "aarch64-darwin"
       ];
 
-      forEachSupportedSystem = f: nixpkgs.lib.genAttrs
-        supportedSystems
-        (
-          system: f {
+      forEachSupportedSystem =
+        f:
+        nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f {
             pkgs = import nixpkgs { inherit system; };
           }
         );
     in
     {
-      devShells = forEachSupportedSystem ({ pkgs }: {
-        default = pkgs.mkShell
-          {
+      devShells = forEachSupportedSystem (
+        { pkgs }:
+        {
+          default = pkgs.mkShell {
             packages = with pkgs; [
               bashInteractive
               cmake
               doxygen
-              llvmPackages_latest.clang-tools
-              llvmPackages_latest.bintools
-              llvmPackages_latest.clang
-              llvmPackages_latest.libclc
-              llvmPackages_latest.libcxx
-              llvmPackages_latest.lldb
-              llvmPackages_latest.lldbPlugins.llef
+              llvmPackages.clang-tools
+              llvmPackages.bintools
+              llvmPackages.clang
+              llvmPackages.libclc
+              llvmPackages.libcxx
+              llvmPackages.lldb
+              llvmPackages.lldbPlugins.llef
               llvmPackages.lldb-manpages
-              llvmPackages_latest.llvm
-              llvmPackages_latest.mlir
-              llvmPackages_latest.openmp
-              llvmPackages_latest.stdenv
+              llvmPackages.llvm
+              llvmPackages.mlir
+              llvmPackages.openmp
+              llvmPackages.stdenv
               ninja
               python3
               vcpkg
               vcpkg-tool
             ];
           };
-      });
+        }
+      );
     };
 }
